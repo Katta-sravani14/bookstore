46d447449b86a86b61829117e7f94a3554aba88f
Katta-sravani14 <77522021+Katta-sravani14@users.noreply.github.com>
2021-05-22 08:15:11 +0530

    added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+import { LoginComponent } from './login/login.component';
+
+
+
+// import { AdminComponent } from './bookstore/admin/admin.component';
+// import { GetallnoteComponent } from './components/getallnote/getallnote.component';
+// import { TrashComponent } from './components/trash/trash.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { DisplayComponent } from './display/display.component';
+// import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
+// import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+
+
+const routes: Routes = [
+ 
+   
+     
+      {
+        path: 'login',
+        component : LoginComponent
+      },
+      
+      {
+        path: 'header',
+        component : HeaderComponent
+      },
+      {
+        path: 'footer',
+        component : FooterComponent
+      },
+      {
+        path: 'dashboard',
+        component : DashboardComponent
+      },
+     
+     
+    ];
+  
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
